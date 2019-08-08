72c42af5f5386e84e409c127e05221397f8baffe
Alexander Nabilsky <Alexander.Nabilsky@intravision.ru>
2019-08-08 15:24:12 +0300

    Refactoring

--- NetCoreWithAngular/HostedServices/HostedService.cs
+++ NetCoreWithAngular/HostedServices/HostedService.cs
@@ -39,41 +39,46 @@ namespace NetCoreWithAngular.HostedServices
         {
             try
             {
-                using (var scope = _serviceProvider.CreateScope())
-                {
-                    var bookRepository = scope.ServiceProvider.GetRequiredService<IRepository<Book, int>>();
-                    bookRepository.CancellationToken = cancellationToken;
-                    if (!(await bookRepository.GetQueryNoTracking().AnyAsync(cancellationToken)))
-                    {
-                        for (int i = 1; i <= 10; i++)
-                        {
-                            await bookRepository.AddAsync(new Book { Title = $"Book{i}", Author = $"Author{i}", PageCount = i });
-                        }
-                        await bookRepository.SaveChangesAsync();
-                    }
-                }
-                
+                var t1 = Task.Run(async () => await CreateAnime(cancellationToken));
+                var t2 = Task.Run(async () => await CreateBooks(cancellationToken));
+
+                await Task.WhenAll(t1, t2);
             }
-            catch { }
-            
-            try
+            catch { }            
+        }
+
+        private async Task CreateBooks(CancellationToken cancellationToken)
+        {
+            using (var scope = _serviceProvider.CreateScope())
             {
-                using (var scope = _serviceProvider.CreateScope())
+                var bookRepository = scope.ServiceProvider.GetRequiredService<IRepository<Book, int>>();
+                bookRepository.CancellationToken = cancellationToken;
+                if (!(await bookRepository.GetQueryNoTracking().AnyAsync(cancellationToken)))
                 {
-                    var animeRepository = scope.ServiceProvider.GetRequiredService<IRepository<Anime, int>>();
-                    animeRepository.CancellationToken = cancellationToken;
-                    if (!(await animeRepository.GetQueryNoTracking().AnyAsync(cancellationToken)))
+                    for (int i = 1; i <= 10; i++)
                     {
-                        for (int i = 1; i <= 10; i++)
-                        {
-                            await animeRepository.AddAsync(new Anime { Title = $"Anime{i}", SeasonCount = (i % 2) + 1 });
-                        }
-                        await animeRepository.SaveChangesAsync();
+                        await bookRepository.AddAsync(new Book { Title = $"Book{i}", Author = $"Author{i}", PageCount = i });
                     }
-                }                
+                    await bookRepository.SaveChangesAsync();
+                }
             }
-            catch { }
-            
+        }
+
+        private async Task CreateAnime(CancellationToken cancellationToken)
+        {
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var animeRepository = scope.ServiceProvider.GetRequiredService<IRepository<Anime, int>>();
+                animeRepository.CancellationToken = cancellationToken;
+                if (!(await animeRepository.GetQueryNoTracking().AnyAsync(cancellationToken)))
+                {
+                    for (int i = 1; i <= 10; i++)
+                    {
+                        await animeRepository.AddAsync(new Anime { Title = $"Anime{i}", SeasonCount = (i % 2) + 1 });
+                    }
+                    await animeRepository.SaveChangesAsync();
+                }
+            }
         }
     }
 }
